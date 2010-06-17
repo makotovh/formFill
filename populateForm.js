c3c4b676fca0add17de40ac5a749213645e90b76
Makoto Hashimoto <makoto.vix@gmail.com>
2010-06-17 19:42:30 -0300

    iniciando o projeto

---
+++ populateForm.js
@@ -0,0 +1,94 @@
+/**
+ * jQuery Form Fill - http://makoto.blog.br/formFill
+ * --------------------------------------------------------------------------
+ *
+ * Licensed under The MIT License
+ * 
+ * @version     0.1
+ * @since       16.06.2010
+ * @author      Makoto Vieira Hashimoto
+ * @link        http://makoto.blog.br/formFill
+ * @twitter     http://twitter.com/makoto_vix
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT 
+ * @package     jQuery Plugins
+ * 
+ * Usage:
+ * --------------------------------------------------------------------------
+ * 
+ *	$('form#formUser').fill(user, "user");
+ *  
+ *  <form id="formUser">
+ *  	<label>Name:</label>
+ *  	<input type="text" name="user.name"/>
+ *  	<label>E-mail:</label>
+ *  	<input type="text" name="user.email"/>
+ *  </form>
+ */
+(function($){
+	
+	$.fn.fill = function (obj, settings) {
+		
+		options = $.extend({}, $.fn.fill.defaults, settings);
+		
+		$this = $(this);
+		
+		$this.find("*").each(function(i, item){
+			if ($(item).is("input") || $(item).is("select") || $(item).is("textarea")) {
+				try {
+					
+					var objName;
+					if (options.styleElementName == "object") {
+						objName = $(item).attr("name").replace(/^[a-z]*[0-9]*[a-z]*\./i, 'obj.');
+					} else if (options.type == "none") {
+						objName = 'obj.' + $(item).attr("name");
+					}
+						
+					
+					var value = eval(objName);
+					
+					switch ($(item).attr("type")) {
+						case "hidden":
+						case "password":
+						case "textarea":
+							$(item).val(value);
+						break;
+
+						case "text":
+							var classe = $(item).attr('class');
+							
+							if (classe && classe.search("hasDatepicker") != -1) {
+								$(item).datepicker('setDate', new Date(parseInt(value)));
+							} else if ($(item).attr("alt") == "double") {
+								$(item).val(value.toFixed(2));
+							} else {
+								$(item).val(value);
+							}
+						break;
+						
+						case "select-one":						
+							$(item).val(value).change();
+						break;
+					}
+				} catch(e) {
+					if (options.debug) {
+						debug(e.message);
+					}
+				}
+				
+			}
+			
+		});
+		
+	};
+	
+	$.fn.fill.defaults = {
+		styleElementName: 'object',	// object | array | none
+		debug: false
+	};
+	
+	function debug(message) {                                                                                            // Throws error messages in the browser console.
+        if (window.console && window.console.log) {
+            window.console.log(message);
+        }
+    };
+})(jQuery);
